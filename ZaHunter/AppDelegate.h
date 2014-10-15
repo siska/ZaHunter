c71887b243a910f7049c16ae7a7d745227d3f4ab
Ryan Siska <rsiska1@gmail.com>
2014-10-15 14:59:30 -0500

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by S on 10/15/14.
+//  Copyright (c) 2014 Ryan Siska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
